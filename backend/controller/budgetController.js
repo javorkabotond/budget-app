2764fe5fb849097d2e4baacf3d1fe399ddacdb67
Botond Javorka <javorka.botond@gmail.com>
2022-04-29 23:29:22 +0200

    Create endpoint: update budget by id

--- backend/controller/budgetController.js
+++ backend/controller/budgetController.js
@@ -47,7 +47,45 @@ exports.getBudgetById = async (request, response) => {
     });
   }
 };
-
+exports.updateBudget = async (request, response) => {
+  const id = request.params.id;
+  const { title, descreption, amount, category, date } = request.body;
+  const findBudgetById = await Budget.findOne({
+    where: {
+      id,
+    },
+  });
+  if (!findBudgetById) {
+    response.status(404).send({
+      message: `Budget with id: ${id} not found`,
+    });
+    return;
+  }
+  if (!title || !descreption || !amount || !category || !date) {
+    response.status(400).send({
+      message: "Value cannot be NULL!",
+    });
+    return;
+  }
+  const updateBudget = await Budget.update(
+    {
+      title,
+      descreption,
+      amount,
+      category,
+      date,
+    },
+    { where: { id: id } }
+  );
+  if (!updateBudget) {
+    response.status(400).send({
+      message: `Budget with id: ${id} failed update`,
+    });
+  }
+  response.status(200).send({
+    message: `Budget with id: ${id} success update`,
+  });
+};
 exports.deleteBudget = async (request, response) => {
   try {
     const id = request.params.id;
--- backend/router/budget.js
+++ backend/router/budget.js
@@ -4,6 +4,7 @@ const budgetService = require("../controller/budgetController");
 router.post("/create", budgetService.insertBudget);
 router.get("/budgets", budgetService.getAllBudgets);
 router.get("/budget/:id", budgetService.getBudgetById);
+router.put("/budget/:id", budgetService.updateBudget);
 router.delete("/budget/:id", budgetService.deleteBudget);
 
 module.exports = router;

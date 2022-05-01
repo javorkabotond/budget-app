3381ae560c26c511258c0b40bb4ce9b2ac4d7b96
Botond Javorka <javorka.botond@gmail.com>
2022-05-01 18:37:15 +0200

    Modify budget model

--- backend/model/budgetModel.js
+++ backend/model/budgetModel.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, Sequelize) => {
         allowNull: false,
       },
       category: {
-        type: Sequelize.ENUM("SHOP", "TRANSFER", "OTHER", "ALL"),
+        type: Sequelize.ENUM("SHOP", "TRANSFER", "OTHER"),
         allowNull: false,
       },
       amount: {

2cf38628b5882c55f6708f5ce9d571ba034d0740
Botond Javorka <javorka.botond@gmail.com>
2022-05-01 11:13:00 +0200

    Modify projetc font family

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import BudgetList from "./components/BudgetList";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+    },
+  });
   return (
     <div className="App">
-      <h1>Budget App</h1>
-      <BudgetList />
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar />
+        <BudgetList />
+      </ThemeProvider>
     </div>
   );
 }
